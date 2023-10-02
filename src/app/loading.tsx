43a7356580d9a71ffa8543d5c3c1ac1ef113a49b
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-02 11:27:02 +1030

    add loading component

---
+++ src/app/loading.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="fixed inset-0 flex flex-col items-center justify-center bg-gray-100">
+      <div className="w-20 h-20 rounded-full border-t-4 border-blue-500 border-solid animate-spin"></div>
+      <h1 className="text-2xl mt-4 font-semibold text-gray-800">Loading...</h1>
+    </div>
+  );
+}
+
+export default Loading;
