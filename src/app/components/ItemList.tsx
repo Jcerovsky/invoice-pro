786f3fa7848986462dae8c54404899e055fd4e32
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-03 18:56:29 +1030

    add item list section to new invoice

--- src/app/components/ItemList.tsx
+++ src/app/components/ItemList.tsx
@@ -11,21 +11,25 @@ function ItemList() {
 
   return (
     <div>
-      <h2>Item List</h2>
+      <h2 className="text-gray-500 font-medium text-2xl mb-4">Item List</h2>
       <InputField labelName={"Item Name"} />
-      <div>
+      <div className="grid grid-cols-4 gap-x-6">
         <InputField labelName={"Qty."} placeholder={"1"} />
         <InputField labelName={"Price"} placeholder={"0"} />
-        <div>
+        <div className="flex flex-col">
           <p className="text-mediumPurple">Total</p>
-          <p className="font-bold">${totalSum}</p>
+          <p className="font-bold mt-auto mb-auto">${totalSum}</p>
         </div>
-        <Image
-          src="/assets/icon-delete.svg"
-          alt="delete-bin-icon"
-          width={12}
-          height={16}
-        />
+        <div className="pt-3 mt-auto mb-auto justify-self-end">
+          <svg width="13" height="16" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M11.583 3.556v10.666c0 .982-.795 1.778-1.777 1.778H2.694a1.777 1.777 0 01-1.777-1.778V3.556h10.666zM8.473
+             0l.888.889h3.111v1.778H.028V.889h3.11L4.029 0h4.444z"
+              fill="#888EB0"
+              className="cursor-pointer hover:fill-red-500 transition-all duration-200 "
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
