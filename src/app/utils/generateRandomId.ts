98c9343a509059e386cd0405a720def797edfed1
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-05 18:30:52 +1030

    amend custom function to perform more efficiently

--- src/app/utils/generateRandomId.ts
+++ src/app/utils/generateRandomId.ts
@@ -1,15 +1,15 @@
 import { IInvoice } from "@/app/context/Context";
 
-export const generateRandomId = (data: IInvoice) => {
+export const generateRandomId = (data: IInvoice[]) => {
   let result = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   const charCount = characters.length;
 
   for (let i = 0; i < 6; i++) {
-    const randomNum = Math.ceil(Math.random() * charCount);
-    result += characters[randomNum];
+    const randomNum = Math.floor(Math.random() * charCount);
+    result += characters.charAt(randomNum);
   }
-  if (result in data) {
+  if (data.some((invoice) => invoice.id === result)) {
     generateRandomId(data);
     return;
   }
