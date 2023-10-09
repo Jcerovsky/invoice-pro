5eb8413e70ba9aeedcacb7ce086fb93392ddc97e
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-09 14:34:18 +1030

    prevent users from marking invoice paid when already paid

--- src/app/components/Invoice.tsx
+++ src/app/components/Invoice.tsx
@@ -100,7 +100,8 @@ function Invoice({ invoiceData }: { invoiceData: IInvoice }) {
               Delete
             </button>
             <button
-              className="px-6 py-2 cursor-pointer rounded-full bg-buttonPurple hover:bg-purple-500 text-white duration-200"
+              className="px-6 py-2 cursor-pointer rounded-full bg-buttonPurple enabled:hover:bg-purple-500 text-white duration-200
+              disabled:opacity-50  disabled:cursor-not-allowed"
               disabled={invoiceData.status === "paid"}
             >
               Mark as Paid
