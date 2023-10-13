7edc0ca1760f722090a5e5ffc0e9a80bb27bc324
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-13 18:20:38 +1030

    create function to send invoice to mongodb database

--- src/app/api/invoices/route.ts
+++ src/app/api/invoices/route.ts
@@ -1,7 +1,15 @@
-import {NextResponse} from "next/server";
+import {NextRequest, NextResponse} from "next/server";
+import connectDB from "@/app/lib/mongodb";
 
-export async function POST(req) {
-    const newInvoiceData = await req.json()
-    console.log('newInvoiceData:',newInvoiceData)
+export async function POST() {
+    try {
+        await connectDB()
+        return NextResponse.json({
+            msg: ['Invoice sent successfully'],
+            success: true,
+        })
+    } catch (error) {
+        return NextResponse.json({msg: ["There was an error - invoice not sent"]})
+    }
     return NextResponse.json({msg: 'hello from invoices route'})
 }
