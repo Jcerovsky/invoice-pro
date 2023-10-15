c62e600588472251101fa4622856de4735ef4676
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-10-15 20:33:11 +1030

    fix invoice filtering and displaying

--- src/app/components/AllInvoices.tsx
+++ src/app/components/AllInvoices.tsx
@@ -5,6 +5,7 @@ import { Context, IInvoice } from "@/app/context/Context";
 import Invoice from "@/app/components/Invoice";
 import InvoiceOverview from "@/app/components/InvoiceOverview";
 import NoInvoices from "@/app/components/NoInvoices";
+import { set } from "zod";
 
 interface ICheckboxProps {
   paid: boolean;
@@ -16,27 +17,24 @@ function AllInvoices() {
   const { allInvoices, checkboxState } = useContext(Context)!;
   const [filteredInvoices, setFilteredInvoices] = useState<IInvoice[]>([]);
 
+  const checkedItems = Object.keys(checkboxState).filter(
+    (key) => checkboxState[key as keyof ICheckboxProps],
+  );
+
   useEffect(() => {
     const handleFilter = () => {
-      const checkedItems = Object.keys(checkboxState).filter(
-        (key) => checkboxState[key as keyof ICheckboxProps],
-      );
-
       setFilteredInvoices(
         allInvoices.filter((invoice) => checkedItems.includes(invoice.status)),
       );
     };
-
     handleFilter();
   }, [checkboxState]);
 
   const invoiceCount =
-    filteredInvoices.length !== 0
-      ? filteredInvoices.length
-      : allInvoices.length;
+    checkedItems.length !== 0 ? filteredInvoices.length : allInvoices.length;
 
   const invoicesToBeDisplayed =
-    filteredInvoices?.length > 0 ? filteredInvoices : allInvoices;
+    checkedItems.length !== 0 ? filteredInvoices : allInvoices;
 
   return (
     <div>
